dd26766353e9e100e0dc962008a7b89b0d37ed04
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-09-12 21:20:15 +0100

    Convert to web app

--- EntityFrameworkTriggersTest/BloggingContext.cs
+++ EntityFrameworkTriggersTest/BloggingContext.cs
@@ -2,22 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkTriggersTest;
 
-public class BloggingContext : DbContext
+public class BloggingContext(DbContextOptions<BloggingContext> options) : DbContext(options)
 {
-    private readonly string _connectionString;
     public DbSet<Blog> Blogs { get; set; }
     public DbSet<Post> Posts { get; set; }
-
-    //Required for ef migrations to work because there is no DI container in this app that it can reference
-    public BloggingContext() { }
-
-    public BloggingContext(string connectionString)
-    {
-        _connectionString = connectionString;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(_connectionString);
-    }
 }
--- EntityFrameworkTriggersTest/Program.cs
+++ EntityFrameworkTriggersTest/Program.cs
@@ -2,21 +2,84 @@
 
 namespace EntityFrameworkTriggersTest;
 
-internal class Program
+public static class Program
 {
-    private static void Main()
+    public static void Main(string[] args)
     {
-        var dbConnectionString = Environment.GetEnvironmentVariable("dbConnectionString") ?? string.Empty;
-
-        Console.WriteLine("Super spike initiating");
-        Console.WriteLine($"DB connection string is \"{dbConnectionString}\"");
-
-        var bloggingContext = new BloggingContext(dbConnectionString);
+        var builder = WebApplication.CreateBuilder(args);
         
+        // Add services to the container.
+        builder.Services.AddAuthorization();
 
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+
+        builder.Services.AddDbContext<BloggingContext>(options =>
+            options.UseSqlServer("name=ConnectionStrings:BloggingDatabase"));
+
+        var app = builder.Build();
+
+        app.Logger.LogInformation("Plop");
+        app.Logger.LogInformation("BloggingDatabase connection string is: " + app.Configuration.GetConnectionString("BloggingDatabase"));
+        
         //Never do this in production code - don't want to risk multiple migrations running at the same time!!
-        bloggingContext.Database.Migrate();
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<BloggingContext>();
+            if (db.Database.GetPendingMigrations().Any())
+            {
+                db.Database.Migrate();
+            }
+        }
+
+        app.UseHealthChecks("/health");
         
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        ConfigureMinimalApis(app);
         
+        app.Run();
+    }
+
+    private static void ConfigureMinimalApis(WebApplication app)
+    {
+        app.MapGet("/blogs", async (BloggingContext db) =>
+            await db.Blogs.ToListAsync())
+            .WithOpenApi();
+
+        app.MapGet("/blogs/{id}", async (int id, BloggingContext db) =>
+            await db.Blogs.FindAsync(id)
+                is { } blog
+                ? Results.Ok(blog)
+                : Results.NotFound());
+
+        app.MapPost("/blogs", async (Blog blog, BloggingContext db) =>
+        {
+            db.Blogs.Add(blog);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/blogs/{blog.BlogId}", blog);
+        });
+
+        app.MapDelete("/blogs/{id}", async (int id, BloggingContext db) =>
+        {
+            if (await db.Blogs.FindAsync(id) is not { } blog) 
+                return Results.NotFound();
+            
+            db.Blogs.Remove(blog);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        });
     }
 }
